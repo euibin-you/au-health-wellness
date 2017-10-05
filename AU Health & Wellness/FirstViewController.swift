8272cfa04a9325037fb6fae35945f47da115c754
Eui Bin You <30663068+euibin-you@users.noreply.github.com>
2017-10-05 16:08:48 -0500

    Initial Commit

---
+++ AU Health & Wellness/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AU Health & Wellness
+//
+//  Created by You, EuiBin on 10/5/17.
+//  Copyright © 2017 You, EuiBin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
